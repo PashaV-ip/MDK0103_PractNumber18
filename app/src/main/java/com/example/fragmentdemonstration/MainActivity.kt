7e2de3f2af40a68024e0ee98ebe8b436016c6fd9
Vanshev <pav-alva@ya.ru>
2022-11-07 00:35:28 +0300

    Pract number 18-19

--- app/src/main/java/com/example/fragmentdemonstration/MainActivity.kt
+++ app/src/main/java/com/example/fragmentdemonstration/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.fragmentdemonstration
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,7 +10,18 @@ class MainActivity : AppCompatActivity(),
         setContentView(R.layout.activity_main)
     }
     override fun onSendData(selectedItem: String?) {
-        val fragment = supportFragmentManager.findFragmentById(R.id.detailFragment) as DetailFragment?
+        val fragment = supportFragmentManager
+            .findFragmentById(R.id.detailFragment) as DetailFragment?
+        if (fragment != null && fragment.isVisible){
+            fragment.setSelectedItem(selectedItem)
+        }
+        else{
+            val intent = Intent(applicationContext,
+                DetailActivity::class.java)
+            intent.putExtra(DetailActivity.SELECTED_ITEM, selectedItem)
+            startActivity(intent)
+        }
         fragment?.setSelectedItem(selectedItem)
     }
 }
+
